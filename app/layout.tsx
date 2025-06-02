00dcb926711c53f60de6e9a4441b893847f37895
bernisjose80 <bernisjose80@gmail.com>
2025-06-02 11:31:15 -0400

    version final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,23 @@
-import '@/app/ui/global.css';
-import { monserrat } from '@/app/ui/fonts';
+import "@/app/ui/global.css";
+import { monserrat } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">      
+    <html lang="en">
       <body className={`${monserrat.className} antialiased`}>{children}</body>
     </html>
   );
